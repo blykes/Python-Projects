131f0067a240d51353a80689386bb0a723db404a
Brian Lykes <blykes@gmail.com>
2017-08-09 23:15:14 -0400

    Did problem 8-3

--- Chapter 8 Functions.py
+++ Chapter 8 Functions.py
@@ -11,3 +11,10 @@ def favorite_book(title):
 
 favorite_book('brave new world')
 
+#8-3 T-Shirt
+def make_shirt(shirt_size, shirt_print):
+    print("My sirt size is " + shirt_size + ".")
+    print("The shirt should say " + shirt_print + ".")
+
+make_shirt('M', 'WELCOME HOME VEN!')
+make_shirt(shirt_size='M', shirt_print='I MISSED YOU!')

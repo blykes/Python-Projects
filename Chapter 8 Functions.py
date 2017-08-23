e495fb954d1b558e2a42091d43beb82d3f9a6df3
Brian Lykes <blykes@gmail.com>
2017-08-23 15:38:32 -0400

    did 8-11

--- Chapter 8 Functions.py
+++ Chapter 8 Functions.py
@@ -123,38 +123,81 @@
 ##print("\nThanks for playing!")
 
 #8-9 Magicians
-def show_magicians(magicians):
-    """Show each magician in a list"""
-    for magician in magicians:
-        print(magician.title())
-
-magicians = ['harry houdini', 'david blaine', 'teller']
-show_magicians(magicians)
+##def show_magicians(magicians):
+##    """Show each magician in a list"""
+##    for magician in magicians:
+##        print(magician.title())
+##
+##magicians = ['harry houdini', 'david blaine', 'teller']
+##show_magicians(magicians)
 
 #8-10 Great Magicians
+##def show_magicians(magicians):
+##    """Print the name of each magician in the list."""
+##    for magician in magicians:
+##        print(magician)
+##        
+##def make_great(magicinans):
+##    #add "the great" to the names
+##    #build a new list to hold the great magicians
+##    great_magicians = []
+##
+##    #making each magigician great
+##    while magicians:
+##        magician = magicians.pop()
+##        great_magician = magician + ' The Great!'
+##        great_magicians.append(great_magician)
+##
+##    # Add the great magicians back into magicians.
+##    for great_magician in great_magicians:
+##        magicians.append(great_magician)
+##
+##magicians = ['Harry Houdini', 'David Blaine', 'Teller']
+##show_magicians(magicians)
+##
+##print("\n")
+##make_great(magicians)
+##show_magicians(magicians)
+
+#8-11 Unchanged magicians
 def show_magicians(magicians):
     """Print the name of each magician in the list."""
     for magician in magicians:
         print(magician)
-        
-def make_great(magicinans):
-    #add "the great" to the names
-    #build a new list to hold the great magicians
+
+def make_great(magicians):
+    """Add 'the Great!' to each magician's name."""
+    # Build a new list to hold the great musicians.
     great_magicians = []
 
-    #making each magigician great
+    # Make each magician great, and add it to great_magicians.
     while magicians:
         magician = magicians.pop()
-        great_magician = magician + ' The Great!'
+        great_magician = magician + ' the Great'
         great_magicians.append(great_magician)
 
     # Add the great magicians back into magicians.
     for great_magician in great_magicians:
         magicians.append(great_magician)
 
+    return magicians
+
 magicians = ['Harry Houdini', 'David Blaine', 'Teller']
 show_magicians(magicians)
 
-print("\n")
-make_great(magicians)
+print("\nGreat magicians:")
+great_magicians = make_great(magicians[:])
+show_magicians(great_magicians)
+
+print("\nOriginal magicians:")
 show_magicians(magicians)
+
+
+
+
+
+
+
+
+
+

4381254b3cd8164d77d576e315470b72ed899fad
Brian Lykes <blykes@gmail.com>
2017-08-27 15:02:59 -0400

    finished chapter 9

--- Chapter 9 Classes.py
+++ Chapter 9 Classes.py
@@ -482,3 +482,68 @@ print("\nThe admin " + eric.username + " has these privileges: ")
 eric.privileges.show_privileges()
 
 #9-13 OrderedDict Rewrite
+from collections import OrderedDict
+
+glossary = OrderedDict()
+
+glossary['string'] = 'A series of characters.'
+glossary['comment'] = 'A note in a program that the Python interpreter ignores.'
+glossary['list'] = 'A collection of items in a particular order.'
+glossary['loop'] = 'Work through a collection of items, one at a time.'
+glossary['dictionary'] = "A collection of key-value pairs."
+glossary['key'] = 'The first item in a key-value pair in a dictionary.'
+glossary['value'] = 'An item associated with a key in a dictionary.'
+glossary['conditional test'] = 'A comparison between two values.'
+glossary['float'] = 'A numerical value with a decimal component.'
+glossary['boolean expression'] = 'An expression that evaluates to True or False.'
+
+for word, definition in glossary.items():
+    print("\n" + word.title() + ": " + definition)
+
+#9-14 Dice
+from random import randint
+x = randint(1, 6)
+
+from random import randint
+
+class Die():
+    """Represent a die, which can be rolled."""
+
+    def __init__(self, sides=6):
+        """Initialize the die."""
+        self.sides = sides
+
+    def roll_die(self):
+        """Return a number between 1 and the number of sides."""
+        return randint(1, self.sides)
+
+# Make a 6-sided die, and show the results of 10 rolls.
+d6 = Die()
+
+results = []
+for roll_num in range(10):
+    result = d6.roll_die()
+    results.append(result)
+print("10 rolls of a 6-sided die:")
+print(results)
+
+# Make a 10-sided die, and show the results of 10 rolls.
+d10 = Die(sides=10)
+
+results = []
+for roll_num in range(10):
+    result = d10.roll_die()
+    results.append(result)
+print("\n10 rolls of a 10-sided die:")
+print(results)
+
+# Make a 20-sided die, and show the results of 10 rolls.
+d20 = Die(sides=20)
+
+results = []
+for roll_num in range(10):
+    result = d20.roll_die()
+    results.append(result)
+print("\n10 rolls of a 20-sided die:")
+print(results)
+

4b63a868f21e31eec5bfdae95dcdea1cdf834934
Brian Lykes <blykes@gmail.com>
2017-09-09 19:52:19 -0400

    built ship class and settings

--- Space_Invasion.py
+++ Space_Invasion.py
@@ -2,13 +2,20 @@
 import sys
 import pygame
 
+from settings import settings
+
 #main function for running game
 def run_game():
     #init game and create object
     pygame.init()
-    screen = pygame.display.set_mode((1200,800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screenwidth, ai_settings.screenheight))
     pygame.display.set_caption("Alien Invasion")
 
+    #set color
+    bg_color = (230, 230, 230)
+    
 
     #Start main loop for game
     while True:
@@ -17,6 +24,10 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        #Redraw scree with each pass through the loop
+            screen.fill.(ai_settings.bg_color)
+            
 
         #Make most recently drawn screen visible
         pygame.display.flip()

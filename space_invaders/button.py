e1da3c81d47bdf89a06805cf70f1c9e52db8ec65
Brian Lykes <blykes@gmail.com>
2018-07-18 19:52:10 -0400

    Finished button file

--- space_invaders/button.py
+++ space_invaders/button.py
@@ -2,7 +2,7 @@ import pygame.font
 
 class Button():
     #Init button attributes
-    def __init__ (self, ai_settings, screen, msg):
+    def __init__(self, ai_settings, screen, msg):
         self.screen = screen
         self.screen_rect = screen.get_rect()
 
@@ -18,6 +18,15 @@ class Button():
 
         #Prep button message once
         self.prep_msg(msg)
-        
-        
-        
+
+    #Renders text as image and centers text in button
+    def prep_msg(self, msg):
+        self.msg_image = self.font.render(msg, True, self.text_color,
+            self.button_color)
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.center = self.rect.center
+
+    #Draw blank button then message
+    def draw_button(self):
+        self.screen.fill(self.button_color, self.rect)
+        self.screen.blit(self.msg_image, self.msg_image_rect)

6f1daa915c7f6a50b17e739992dcf935044eda3c
Brian Lykes <blykes@gmail.com>
2018-07-18 19:54:34 -0400

    Added play button draw to main game file

--- space_invaders/space_invasion.py
+++ space_invaders/space_invasion.py
@@ -5,6 +5,7 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings
 from game_stats import GameStats
+from button import button
 from ship import Ship
 import game_functions as gf
 
@@ -16,6 +17,9 @@ def run_game():
 	screen = pygame.display.set_mode(
 		(ai_settings.screen_width, ai_settings.screen_height))
 	pygame.display.set_caption("Space Invasion")
+
+	#Make "play" button
+	play_button = Button(ai_settings, screen, "Play")
 
 	#Creats stats tracking
 	stats = GameStats(ai_settings)
@@ -45,6 +49,7 @@ def run_game():
 			gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
 			gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
 
-		gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+		gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets,
+                                 play_button)
 
 run_game()

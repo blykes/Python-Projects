10c676ede31dfd9b357e6ede138e1d1dd54be423
Brian Lykes <blykes@gmail.com>
2018-07-18 21:07:23 -0400

    Began Scoring system

--- space_invaders/game_stats.py
+++ space_invaders/game_stats.py
@@ -10,11 +10,15 @@ class GameStats():
         self.game_active = False
 
         #Start game in active state
-        self.game_active = True
+        #self.game_active = True
+
+        #High score
+        self.high_score = 0
 
     def reset_stats(self):
         #Initialze stats that can change during gameplay
         self.ships_left = self.ai_settings.ship_limit
-        
+        self.score = 0
+        self.level = 1
 
 
--- space_invaders/settings.py
+++ space_invaders/settings.py
@@ -9,27 +9,46 @@ class Settings():
         self.bg_color = (230, 230, 230)
 
         # Ship settings.
-        self.ship_speed_factor = 1.5
+  #      self.ship_speed_factor = 1.5
         self.ship_limit = 3        
 
         # Bullet settings.
-        self.bullet_speed_factor = 3
+ #       self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
 
         #Alien setings
-        self.alien_speed_factor = 1
+ #       self.alien_speed_factor = 1
         self.fleet_drop_speed = 10
 
+        #How quickly game speeds up
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
         #fleet direction of 1 represents right while -1 reps left
+ #       self.fleet_direction = 1
+
+        
+    #Settings that change throughout game
+    def initialize_dynamic_settings(self):
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1
+        
+        # Scoring.
+        self.alien_points = 50
+    
+        # fleet_direction of 1 represents right, -1 represents left.
         self.fleet_direction = 1
 
-    
+    #Increase speed settings and point valuse as level increases
+    def increase_speed(self):
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
         
-    
-        
-
-    
+        self.alien_points = int(self.alien_points * self.score_scale)
         
